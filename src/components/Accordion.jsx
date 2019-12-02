52cb85c32e2bfa230b8a7e326935a7d12b5b01ad
Arohan Dutt <arohandutt@gmail.com>
2019-12-01 23:18:22 -0600

    Add header overlay

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const defaultItems = [
+  'Sriracha normcore snackwave',
+  'listicle meditation',
+  'williamsburg poke taxidermy',
+  'tumblr chia',
+  'Ugh plaid irony',
+  'keffiyeh craft beer',
+  'selvage hammock humblebrag',
+  'leggings williamsburg',
+  'direct trade lomo mlkshk',
+  'Disrupt bushwick'
+]
 
 const Accordion = () => {
+
+  const [items, setItems] = useState(defaultItems)
+
   return (
     <div className={'accordion_container'}>
-      <h3>Don't forget to bring...</h3>
+      <p className={'accordion_header'}>Don't forget to bring...</p>
+      <div className={'accordion_list'}>
+        <p className={'accordion_list_header'}>This is an accordion to show/hide lists</p>
+        <ul className={'accordion_list_holder'}>
+          {
+            defaultItems.map(item => <li className={'accordion_list_item'}>{item}</li>)
+          }
+        </ul>
+      </div>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
     <div className={'info_header_container'}>
       <div>
         <img className={'info_header_img'} src='./Media/0428AR_2356GV_1.jpg' ></img>
+        <div className={'info_header_overlay'}></div>
         <h1 className={'info_header'}>Plan your Disney Vacation with us!</h1>
       </div>
     </div>
