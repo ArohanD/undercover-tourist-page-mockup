37ea8356b8ac2569e69b4c3985145fa3c7a6f867
Arohan Dutt <arohandutt@gmail.com>
2019-12-02 00:46:52 -0600

    Style info_about section and button

--- src/components/Info_About.jsx
+++ src/components/Info_About.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
+const dummyText = `Sriracha normcore snackwave listicle meditation, williamsburg poke taxidermy tumblr chia. Ugh plaid irony, keffiyeh craft beer selvage hammock humblebrag leggings williamsburg direct trade lomo mlkshk. Disrupt bushwick next level shaman kombucha pok pok meggings art party adaptogen affogato. Typewriter helvetica umami master cleanse palo santo. +1 taxidermy shabby chic drinking vinegar kale chips tofu. Slow-carb schlitz gentrify art party cold-pressed kale chips flexitarian disrupt distillery banjo normcore skateboard four dollar toast. Cloud bread hashtag four dollar toast drinking vinegar farm-to-table chartreuse quinoa biodiesel brunch waistcoat jianbing squid butcher. `
+
 const Info_About = () => {
   return (
     <div className={'info_about_container'}>
       <p className={'info_about_h1'}>Packing for your Orlando Trip?</p>
       <p className={'info_about_h2'}>We can help you get ready.</p>
-      <p>Tilde chillwave XOXO tattooed. VHS semiotics messenger bag chartreuse irony cold-pressed ethical 3 wolf moon pop-up actually jianbing 8-bit pabst gentrify. Freegan kinfolk lyft raw denim, green juice mustache banjo vaporware bicycle rights PBR&B try-hard everyday carry meh. Heirloom pork belly food truck glossier retro selvage flannel lomo plaid adaptogen snackwave williamsburg aesthetic.</p>
-      <p>Keytar waistcoat wolf snackwave, iceland gastropub tacos asymmetrical post-ironic YOLO marfa. Skateboard stumptown jean shorts chicharrones, iceland pabst YOLO. Four loko slow-carb organic cliche scenester quinoa edison bulb forage etsy vegan. Hashtag crucifix tote bag etsy literally raw denim mlkshk chicharrones vaporware offal fashion axe yuccie. Poutine everyday carry blue bottle pickled raw denim subway tile.</p>
-      <button>Continue Reading</button>
+      <p className={'info_about_text'}>{dummyText}</p>
+      <button className={'info_button'}>Continue Reading</button>
     </div>
   )
 }
